310a40a53c818e16f09bb7b8cee461298635a105
manohar-darsi <darsimanohar2@gmail.com>
2025-03-16 14:03:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let container = document.createElement("div")
+container.className="container"
+function fetchData(){
+    fetch("https://fakestoreapi.com/products")
+    .then(Response=>Response.json())
+    .then(data=>displayData(data))
+    // .catch(err=>console.error(err))
+}
+function displayData(data){
+    for(let obj of data){
+    let item = document.createElement("div")
+    item.className = "item"
+    item.innerHTML=`
+    <img class= "image" src="${obj.image}">
+    <p class = "title">"${obj.title}"</p>-<span><b>"${obj.price}"</b></span>
+    <p class ="category"><i><b>"${obj.category}"</i></b></p>
+    <p class ="rating">"${obj.rating.rate}"⭐</p>
+    ` 
+        container.appendChild(item);
+    }
+    
+    document.body.appendChild(container)
+    document.querySelector(".loader").remove();
+}
+
+fetchData()
+
